6e8de3ae94e411579fa021878c5fce1c963fb2de
Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>
2013-06-14 14:28:11 +0800

    Initialize user model
    Created a standard user model with extra 'karma' field to store
    user points.
    
    Signed-off-by: Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ActiveRecord::Base
+  before_save :create_remember_token
+  before_save { self.email.downcase! }
+
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+    length: { maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+    format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+end
