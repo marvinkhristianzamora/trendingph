800b4b53504a7a5ca44f77ed0323599e933f38a3
Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>
2013-06-14 12:56:24 +0800

    Bootstrap the home page
    Signed-off-by: Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  let(:title) { 'Trending PH' }
+  subject { page }
+
+  shared_examples_for 'all static pages' do
+    it { should have_content(heading) }
+    it { should have_title(page_title) }
+  end
+
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading) { "#{title}" }
+    let(:page_title) { "" }
+
+    it_should_behave_like "all static pages"
+  end
+end
