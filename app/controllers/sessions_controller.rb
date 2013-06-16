c8f96851dbf62769f22155879fa4c32accf47b7f
Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>
2013-06-16 19:51:13 +0800

    Implement sessions controller
    The controller includes the server side sign in/out
    implementation.
    
    Signed-off-by: Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(username: params[:session][:username].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_back_or root_path
+    else
+      flash.now[:error] = "Invalid email/password combo!"
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to root_path
+  end
+
+
+end
