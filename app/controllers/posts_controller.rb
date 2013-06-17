b8774031b40432b9fbb74b1333ab93a7325efe1b
Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>
2013-06-17 22:01:09 +0800

    Implement post creation in Post controller
    Signed-off-by: Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,22 @@
 class PostsController < ApplicationController
-  before_action :signed_in_user, only: [:new]
+  before_action :signed_in_user, only: [:new, :create]
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = "Posted!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:url, :title, :content)
+    end
 end
--- spec/requests/post_pages_spec.rb
+++ spec/requests/post_pages_spec.rb
@@ -13,4 +13,23 @@ describe "PostPages" do
 
     it { should have_title("Submit") }
   end
+
+  describe "post creation" do
+    before do
+      valid_signin user 
+      visit new_post_path
+    end
+
+    describe "with invalid information" do
+      it "should not create new post" do
+        expect { click_button "Submit" }.not_to change(Post, :count)
+      end
+    end
+
+    describe "error message" do
+      before { click_button "Submit" }
+
+      it { should have_content("error") }
+    end
+  end
 end
