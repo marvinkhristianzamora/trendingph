23aca4e6aec0bad6f06eabd201e7bd19bf0785a3
Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>
2013-06-17 17:35:15 +0800

    Fix signin helper to user user.username instead of email
    Signed-off-by: Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -2,7 +2,7 @@ include ApplicationHelper
 
 def valid_signin(user)
   visit signin_path
-  fill_in "Username", with: user.email
+  fill_in "Username", with: user.username
   fill_in "Password", with: user.password
   click_button "Sign in"
   cookies[:remember_token] = user.remember_token
