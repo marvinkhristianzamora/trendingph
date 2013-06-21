782498344e9991aa1b44385dc58a747b8e104349
Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>
2013-06-21 13:55:23 +0800

    Add tests for post index page
    Signed-off-by: Marvin Khristian Zamora <marvinlz@ph.ncs-i.com>

--- spec/requests/post_pages_spec.rb
+++ spec/requests/post_pages_spec.rb
@@ -32,4 +32,27 @@ describe "PostPages" do
       it { should have_content("error") }
     end
   end
+
+  describe "post index" do
+
+    describe "list" do
+      let!(:post1) { FactoryGirl.create(:post, user: user, url: "http://url1.com",
+                                       title: "Title1", upvote: 10 ) }
+      let!(:post2) { FactoryGirl.create(:post, user: user, url: "http://url2.com",
+                                       title: "Title2", upvote: 20 ) }
+      before do
+        @posts = Post.all.to_a
+        puts Post.all.to_a
+        visit root_path
+      end
+
+      it "should have saved posts" do
+        @posts.each do |post|
+          expect(page).to have_link(post.title, href: post.url)
+          expect(page).to have_link(post.user.username, href: user_path(post.user))
+          expect(page).to have_content(post.upvote)
+        end
+      end
+    end
+  end
 end
